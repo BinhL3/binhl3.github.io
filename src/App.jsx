baea16709a6d52fb78508618e4e2c2dc7d033122
bql23 <bql23@drexel.edu>
2024-09-28 23:48:53 -0400

    - HIHI

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,105 @@
-import Header from './Header'
-import Footer from './Footer'
-import Card from './Card';
-import React, { useState, useEffect } from 'react';
+import Header from "./Header";
+import Footer from "./Footer";
+import Card from "./Card";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return(
+  return (
     <div className="main-container">
       <Header></Header>
-      <img className="header-image"src="assets/nycsubway.jpg"></img>
-      <p>Actively looking for SWE opportunities! Interested in back-end development, machine learning, and content creation.</p>
-      <hr style={{border: 'none', height: '1px', backgroundColor: 'black'}} />
+      <img className="header-image" src="assets/nycsubway.jpg"></img>
+      <p>
+        Actively looking for SWE opportunities! Interested in back-end
+        development, machine learning, and content creation.
+      </p>
+      <p>IT'S MY BIRTHDAY!!! 9/29/2003</p>
+      <hr style={{ border: "none", height: "1px", backgroundColor: "black" }} />
 
-    <div className="experience" style={{marginBottom: '1.75em'}}>
-        <img src="assets/drexel.png" alt="Drexel"/>
+      <div className="experience" style={{ marginBottom: "1.75em" }}>
+        <img src="assets/drexel.png" alt="Drexel" />
         <div>
-            <h2>Drexel University <span className="time">12/2025</span></h2>
-            <p>Bachelor of Science in Computer Science</p>
+          <h2>
+            Drexel University <span className="time">12/2025</span>
+          </h2>
+          <p>Bachelor of Science in Computer Science</p>
         </div>
-    </div>
+      </div>
 
-    <div className="experience">
-        <img src="assets/google.png" alt="Google"/>
+      <div className="experience">
+        <img src="assets/google.png" alt="Google" />
         <div>
-            <h2>Google <span className="time"> 6/2024 — 9/2024</span> </h2>
-            <p>Software Engineer Fellow</p>
+          <h2>
+            Google <span className="time"> 6/2024 — 9/2024</span>{" "}
+          </h2>
+          <p>Software Engineer Fellow</p>
         </div>
-    </div>
+      </div>
 
-    <div className="experience">
-        <img src="assets/dataing.jpg" alt="Dataing"/>
+      <div className="experience">
+        <img src="assets/dataing.jpg" alt="Dataing" />
         <div>
-            <h2>Dataing <span className="time"> 4/2024 — 9/2024</span></h2>
-            <p>Software Engineer Intern</p>
+          <h2>
+            Dataing <span className="time"> 4/2024 — 9/2024</span>
+          </h2>
+          <p>Software Engineer Intern</p>
         </div>
-    </div>
+      </div>
 
-    <div className="experience">
-        <img src="assets/cci.png" alt="Drexel CCI"/>
+      <div className="experience">
+        <img src="assets/cci.png" alt="Drexel CCI" />
         <div>
-            <h2>Drexel University <span className="time"> 9/2024 —</span></h2>
-            <p>Teaching Assistant</p>
+          <h2>
+            Drexel University <span className="time"> 9/2024 —</span>
+          </h2>
+          <p>Teaching Assistant</p>
         </div>
-    </div>
+      </div>
 
-    <div className="experience">
-        <img src="assets/soar.jpg" alt="SOAR Lab"/>
+      <div className="experience">
+        <img src="assets/soar.jpg" alt="SOAR Lab" />
         <div>
-            <h2>SOAR Lab <span className="time"> 3/2024 — 10/2024</span></h2>
-            <p>Research Engineer</p>
+          <h2>
+            SOAR Lab <span className="time"> 3/2024 — 10/2024</span>
+          </h2>
+          <p>Research Engineer</p>
         </div>
-    </div>
-    <div className="experience">
-        <img src="assets/stanford.webp" alt="Stanford University"
-            />
+      </div>
+      <div className="experience">
+        <img src="assets/stanford.webp" alt="Stanford University" />
         <div>
-            <h2>Stanford University <span className="time"> 4/2024 — 6/2024</span></h2>
-            <p>Section Leader</p>
+          <h2>
+            Stanford University <span className="time"> 4/2024 — 6/2024</span>
+          </h2>
+          <p>Section Leader</p>
         </div>
-    </div>
-    <div className="experience">
-        <img src="assets/meta.png" alt="Meta"/>
+      </div>
+      <div className="experience">
+        <img src="assets/meta.png" alt="Meta" />
         <div>
-            <h2>Meta <span className="time"> 1/2024 — 2/2024</span></h2>
-            <p>Software Engineer Fellow</p>
+          <h2>
+            Meta <span className="time"> 1/2024 — 2/2024</span>
+          </h2>
+          <p>Software Engineer Fellow</p>
         </div>
-    </div>
-    <div className="experience">
-        <img src="assets/vnpt.webp" alt="VNPT"/>
+      </div>
+      <div className="experience">
+        <img src="assets/vnpt.webp" alt="VNPT" />
         <div>
-            <h2>VNPT Technology <span className="time"> 4/2022 — 6/2022</span></h2>
-            <p>Software Engineer Intern</p>
+          <h2>
+            VNPT Technology <span className="time"> 4/2022 — 6/2022</span>
+          </h2>
+          <p>Software Engineer Intern</p>
         </div>
-    </div>
-     
-    <div className='resume'>
-    <a href="assets/BinhLeResume.pdf" target="_blank">view full resume</a>
-    </div>
-    <Footer></Footer>
+      </div>
+
+      <div className="resume">
+        <a href="assets/BinhLeResume.pdf" target="_blank">
+          view full resume
+        </a>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
 
-export default App
+export default App;
